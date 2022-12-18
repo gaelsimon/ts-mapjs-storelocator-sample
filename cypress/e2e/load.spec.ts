a49e44adfb52360c765c349f5a4475a6cecfd051
gael <gsimon@webgeoservices.com>
2022-12-18 13:43:52 +0100

    test: fix cypress config

--- cypress/e2e/load.spec.ts
+++ cypress/e2e/load.spec.ts
@@ -8,14 +8,25 @@ const localitiesJSUrl = `${Urls.localitiesWidgetJS}?key=${WoosmapPublicKey}`;
 
 describe('Opening Store Locator', () => {
     before(() => {
+        cy.intercept('GET', 'https://api.woosmap.com/**/*')
+            .as('woosmapAPI');
+
         cy.visit('http://localhost:1234/')
     })
     it('Check the scripts loading', () => {
+        // Verify initial map load successful.
+        cy.wait('@woosmapAPI')
+            .then((interception) => {
+                expect(interception.response?.statusCode).to.equal(200);
+            });
+        cy.wait(2000);
+
         it('Check Map JS Library', () => {
             cy.get(`head script[src="${mapJSUrl}"]`).should('exist');
         })
         it('Check Localities JS Library', () => {
             cy.get(`head script[src="${localitiesJSUrl}"]`).should('exist');
         })
+
     })
 })
