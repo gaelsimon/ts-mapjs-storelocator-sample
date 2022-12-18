e35ceaaa3108c64c21e7a128ae420db9b0be39da
gael <gsimon@webgeoservices.com>
2022-12-18 13:59:45 +0100

    test: fix cypress config

--- cypress/e2e/geolocate.spec.ts
+++ cypress/e2e/geolocate.spec.ts
@@ -7,9 +7,22 @@ describe('Geolocation Use Cases', () => {
     before(() => {
         cy.intercept('GET', 'https://api.woosmap.com/**/*')
             .as('woosmapAPI');
+        cy.intercept('GET', 'https://sdk.woosmap.com/**/*')
+            .as('woosmapSDK');
+
         cy.visitWithGeolocation("http://localhost:1234", userLatLng);
     })
     it(`Geolocate the user on click search__geolocateBtn and display Nearby Stores`, () => {
+        // Verify initial map load successful.
+        cy.wait('@woosmapAPI')
+            .then((interception) => {
+                expect(interception.response?.statusCode).to.equal(200);
+            });
+        cy.wait('@woosmapSDK')
+            .then((interception) => {
+                expect(interception.response?.statusCode).to.equal(200);
+            });
+        cy.wait(5000);
         cy.get(`#${Selectors.searchWrapperID}`)
             .find('.search__geolocateBtn')
             .click()
